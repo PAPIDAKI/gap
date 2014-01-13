c4758fb049b5f5d5b2edaa65af4a7f4154c67ef7
PAPIDAKI <papidakis@gmail.com>
2014-01-13 21:23:42 +0200

    change  cols types for Postgre db

--- db/migrate/20131218131626_change_coloumn_types_to_line.rb
+++ db/migrate/20131218131626_change_coloumn_types_to_line.rb
@@ -1,7 +1,7 @@
 class ChangeColoumnTypesToLine < ActiveRecord::Migration
   def change
-  	change_column :lines, :pmu_id,:integer,'integer USING CAST(pmu_id AS integer)'
+  	change_column :lines, :pmu_id,:integer,'USING CAST(column_name AS integer)'
 
-  	change_column :lines, :sub_id,:integer,'integer USING CAST(sub_id AS integer)'
+  	change_column :lines, :sub_id,:integer,'USING CAST(column_name AS integer)'
   end
 end
