d3f05027e925f9d3e191861703dd739e6f510d76
PAPIDAKI <papidakis@gmail.com>
2014-01-22 22:10:19 +0200

    Fixed orphaned migrations

--- db/migrate/20140116083430_create_facilitations.rb
+++ db/migrate/20140116083430_create_facilitations.rb
@@ -1,8 +1,10 @@
 class CreateFacilitations < ActiveRecord::Migration
   def change
+
     create_table :facilitations do |t|
       t.integer :pmu_id
       t.integer :facility_id
+      
 
       t.timestamps
     end
